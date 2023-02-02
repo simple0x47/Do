48a23c74271873952ce819d44e4cd54dd2017a82
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-02-02 17:11:18 +0200

    + environment based configuration

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: false,
+    auth0: {
+        domain: 'simpleg.eu.auth0.com',
+        clientId: 'uqMJSRrDx91M7JmsaJ6OCeZfuOzxRoPY'
+    }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: true,
+    auth0: {
+        domain: 'simpleg.eu.auth0.com',
+        clientId: 'uqMJSRrDx91M7JmsaJ6OCeZfuOzxRoPY'
+    }
+};
