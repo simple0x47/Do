fd1b308b98d144afc216d17b476e872a38e9f1e1
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-02-03 14:54:23 +0200

    + auth0 tenant management api url

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,5 +3,6 @@ export const environment = {
     auth0: {
         domain: 'simple0x47.eu.auth0.com',
         clientId: 'ahkH71itDim4FV8YXD9HzbIwkWZ3xULg',
+        management_api_url: 'https://simple0x47.eu.auth0.com/api/v2/'
     }
 };
