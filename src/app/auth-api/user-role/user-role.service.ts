e07b3dee2289160ef3b9007d0ba6df665507666a
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-02-03 22:34:47 +0200

    r 'let' to 'const' & optimize import

--- src/app/auth-api/user-role/user-role.service.ts
+++ src/app/auth-api/user-role/user-role.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AuthApiModule } from '../auth-api.module';
 import { AuthService } from '@auth0/auth0-angular';
-import { Observable, mergeMap } from 'rxjs';
+import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 
@@ -21,19 +21,19 @@ type RolesResponse = RoleResponseItem[];
 })
 export class UserRoleService {
   public readonly isAdmin$: Observable<boolean> = new Observable<boolean>(observer => {
-    let userSubscription = this.auth.user$.subscribe(user => {
+    const userSubscription = this.auth.user$.subscribe(user => {
       if (!user || user.sub === undefined) {
         observer.next(false);
         return;
       }
 
       // Request the user's roles by authenticating Auth0's API token.
-      let rolesResponseSubscription = this.http.get<RolesResponse>(`${environment.auth0.management_api_url}users/${user.sub}/roles`, {
+      const rolesResponseSubscription = this.http.get<RolesResponse>(`${environment.auth0.management_api_url}users/${user.sub}/roles`, {
         headers: {
           Authorization: `Bearer ${environment.auth0.management_api_token}`
         }
       }).subscribe(rolesResponse => {
-        for (let role of rolesResponse) {
+        for (const role of rolesResponse) {
           if (role.name === 'admin') {
             observer.next(true);
             return;
