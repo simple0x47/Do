ed92293554b9fee26cd7dcc37ff702c7a8145c38
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-02-03 14:25:51 +0200

    + public keys used for signing the access tokens

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,7 @@ export const environment = {
     production: true,
     auth0: {
         domain: 'simple0x47.eu.auth0.com',
-        clientId: 'ahkH71itDim4FV8YXD9HzbIwkWZ3xULg'
+        clientId: 'ahkH71itDim4FV8YXD9HzbIwkWZ3xULg',
+        jwks: 'https://simple0x47.eu.auth0.com/.well-known/jwks.json'
     }
 };
