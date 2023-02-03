05f79676aaed008db1ebfd2093ff020566752c12
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-02-03 16:23:39 +0200

    - environment files

--- src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-    production: true,
-    auth0: {
-        domain: 'simple0x47.eu.auth0.com',
-        clientId: 'ahkH71itDim4FV8YXD9HzbIwkWZ3xULg',
-        management_api_url: 'https://simple0x47.eu.auth0.com/api/v2/'
-    }
-};
