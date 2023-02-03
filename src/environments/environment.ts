effbd1c9d171f18c76452c9fc431cf6d5f7b9d1d
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-02-03 14:45:00 +0200

    - jwt public signing keys

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,5 @@ export const environment = {
     auth0: {
         domain: 'simple0x47.eu.auth0.com',
         clientId: 'ahkH71itDim4FV8YXD9HzbIwkWZ3xULg',
-        jwks: 'https://simple0x47.eu.auth0.com/.well-known/jwks.json'
     }
 };
