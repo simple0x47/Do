f638963bd6ce23578ee31b29fff0fd497beeb3c5
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-02-04 16:11:13 +0200

    + tasks component with routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { AuthModule } from './auth/auth.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AuthModule } from './auth/auth.module';
+import { TaskModule } from './task/task.module';
 
 @NgModule({
   declarations: [
@@ -14,8 +16,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AuthModule,
     BrowserAnimationsModule,
+    AuthModule,
+    TaskModule
   ],
   providers: [],
   bootstrap: [AppComponent]
