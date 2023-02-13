e7bbce7052da81658062bcb9b465d79b3d819b21
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-02-13 22:44:20 +0200

    + assure component gets created

--- src/app/admin/user-view-dialog/user-view-dialog.component.spec.ts
+++ src/app/admin/user-view-dialog/user-view-dialog.component.spec.ts
@@ -1,6 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserViewDialogComponent } from './user-view-dialog.component';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { UserActionsViewComponent } from '../user-actions-view/user-actions-view.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { User } from '../user';
 
 describe('UserViewDialogComponent', () => {
   let component: UserViewDialogComponent;
@@ -8,12 +15,28 @@ describe('UserViewDialogComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ UserViewDialogComponent ]
+      declarations: [
+        UserViewDialogComponent,
+        UserActionsViewComponent
+      ],
+      imports: [
+        MatDialogModule,
+        MatCheckboxModule,
+        MatCardModule,
+        MatExpansionModule,
+        BrowserAnimationsModule
+      ],
+      providers: [
+        { provide: MAT_DIALOG_DATA, useValue: {} },
+        { provide: MatDialogRef, useValue: {} }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
 
+    TestBed.createComponent(UserActionsViewComponent);
     fixture = TestBed.createComponent(UserViewDialogComponent);
     component = fixture.componentInstance;
+    component.data.user = new User("1", "test", "test@gmail.com", true, []);
     fixture.detectChanges();
   });
 
