6ac9f0ffde5affa0ba5491a91930c3b0d922fea9
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-02-13 22:09:49 +0200

    + test there is a row per user

--- src/app/admin/user-list/user-list.component.spec.ts
+++ src/app/admin/user-list/user-list.component.spec.ts
@@ -1,6 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserListComponent } from './user-list.component';
+import { StoreModule } from '@ngrx/store';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatRow, MatTableModule } from '@angular/material/table';
+import { Observable } from 'rxjs';
+import { User } from '../user';
+import { ActionType, TaskAction } from 'src/app/task-action';
+import { By } from '@angular/platform-browser';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
 
 describe('UserListComponent', () => {
   let component: UserListComponent;
@@ -8,9 +17,16 @@ describe('UserListComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ UserListComponent ]
+      declarations: [UserListComponent],
+      imports: [
+        MatDialogModule,
+        MatTableModule,
+        MatCheckboxModule,
+        MatIconModule,
+        StoreModule.forRoot()
+      ]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(UserListComponent);
     component = fixture.componentInstance;
@@ -20,4 +36,26 @@ describe('UserListComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should list every user', () => {
+    component.users$ = new Observable(observer => {
+      let users: User[] = [
+        new User("1 TEST", "Test Tester", "test@tester.com", false, []),
+        new User("2 TEST", "John Doe", "john@doe.com", true, []),
+        new User("3491913", "19309 15193", "e4913049@219021.com", false, [new TaskAction(ActionType.CREATE, "{}")])
+      ];
+
+      observer.next(users);
+      observer.complete();
+    });
+
+    component.users$.subscribe(users => {
+      component.dataSource.data = users;
+    });
+
+    fixture.detectChanges();
+
+    // One row per user.
+    expect(fixture.debugElement.queryAll(By.directive(MatRow)).length).toBe(3);
+  })
 });
