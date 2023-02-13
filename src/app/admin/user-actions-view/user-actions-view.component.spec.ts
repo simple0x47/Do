252a6536f49d8abf4d0fb9154ce29f39e63d8099
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-02-13 21:53:53 +0200

    + test one card per action

--- src/app/admin/user-actions-view/user-actions-view.component.spec.ts
+++ src/app/admin/user-actions-view/user-actions-view.component.spec.ts
@@ -1,6 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserActionsViewComponent } from './user-actions-view.component';
+import { User } from '../user';
+import { TaskAction } from 'src/app/task-action';
+import { By } from '@angular/platform-browser';
+import { MatCard, MatCardModule } from '@angular/material/card';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('UserAnalyticsViewComponent', () => {
   let component: UserActionsViewComponent;
@@ -8,7 +14,14 @@ describe('UserAnalyticsViewComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [UserActionsViewComponent]
+      declarations: [
+        UserActionsViewComponent
+      ],
+      imports: [
+        MatCardModule,
+        MatExpansionModule,
+        BrowserAnimationsModule
+      ]
     })
       .compileComponents();
 
@@ -19,5 +32,13 @@ describe('UserAnalyticsViewComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should create one action item per action', () => {
+    let actions = [new TaskAction(0, "{}"), new TaskAction(1, "{}")];
+    component.user = new User("test", "test", "test", false, actions);
+    fixture.detectChanges();
+
+    expect(fixture.debugElement.queryAll(By.directive(MatCard)).length).toBe(2);
   });
 });
