66be4c740983a61c2a94f07f7515174498966332
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-02-13 22:51:15 +0200

    f missing dependencies

--- src/app/admin-api/user.service.spec.ts
+++ src/app/admin-api/user.service.spec.ts
@@ -1,12 +1,22 @@
 import { TestBed } from '@angular/core/testing';
 
 import { UserService } from './user.service';
+import { HttpClientModule } from '@angular/common/http';
+import { AuthService } from '@auth0/auth0-angular';
 
 describe('UserService', () => {
   let service: UserService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientModule
+      ],
+      providers: [
+        UserService,
+        { provide: AuthService, useValue: {} }
+      ]
+    });
     service = TestBed.inject(UserService);
   });
 
