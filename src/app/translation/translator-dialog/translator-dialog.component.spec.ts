e6e85c0a63e2705da8f689e7a7c3f032df0b1487
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-02-14 00:15:57 +0200

    f missing dependencies

--- src/app/translation/translator-dialog/translator-dialog.component.spec.ts
+++ src/app/translation/translator-dialog/translator-dialog.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TranslatorDialogComponent } from './translator-dialog.component';
+import { MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
+import { StoreModule } from '@ngrx/store';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('TranslatorDialogComponent', () => {
   let component: TranslatorDialogComponent;
@@ -8,9 +13,19 @@ describe('TranslatorDialogComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TranslatorDialogComponent ]
+      declarations: [TranslatorDialogComponent],
+      imports: [
+        MatDialogModule,
+        MatFormFieldModule,
+        MatSelectModule,
+        BrowserAnimationsModule,
+        StoreModule.forRoot()
+      ],
+      providers: [
+        { provide: MAT_DIALOG_DATA, useValue: {} }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(TranslatorDialogComponent);
     component = fixture.componentInstance;
