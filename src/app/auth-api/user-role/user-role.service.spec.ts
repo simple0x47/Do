9304ec9bac59dd6f1ecad23972a137e3af48e25a
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-02-13 23:34:25 +0200

    f missing dependencies

--- src/app/auth-api/user-role/user-role.service.spec.ts
+++ src/app/auth-api/user-role/user-role.service.spec.ts
@@ -1,12 +1,20 @@
 import { TestBed } from '@angular/core/testing';
 
 import { UserRoleService } from './user-role.service';
+import { HttpClient } from '@angular/common/http';
+import { AuthService } from '@auth0/auth0-angular';
 
 describe('RoleService', () => {
   let service: UserRoleService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClient],
+      providers: [
+        UserRoleService,
+        { provide: AuthService, useValue: {} }
+      ]
+    });
     service = TestBed.inject(UserRoleService);
   });
 
